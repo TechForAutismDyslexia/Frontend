21520888f01c8b29f52984805c0dfb8a9c18c6f0
Shiva Sai Charan Dodda <shivasaicharandodda@gmail.com>
2024-07-02 21:45:16 +0530

    new

--- src/Components/Admin.jsx
+++ src/Components/Admin.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 export default function Admin() {
   const [children, setChildren] = useState([]);
