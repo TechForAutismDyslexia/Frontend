0e7392cc295336a6f38173fdb6a688e705e18b44
ELECTROMONKEY24 <yssmc24@gmail.com>
2024-07-02 19:24:12 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import logoTree from './images/logoTree.jpg';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import logoTree from './images/logoTree.jpg'
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
     const [user, setUser] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
-
     useEffect(() => {
         const token = sessionStorage.getItem('logintoken');
         if (token) {
             setUser(true);
         }
     }, [location]);
-
     const handleLogout = () => {
         sessionStorage.removeItem('logintoken');
         sessionStorage.removeItem('role');
@@ -21,7 +21,6 @@ export default function Navbar() {
         navigate('/login');
         setUser(false);
     };
-
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
@@ -44,27 +43,32 @@ export default function Navbar() {
                             <Link className="nav-link" to="/feedback">Feedback</Link>
                         </li>
                     </ul>
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                                </svg>
-                                <p className="ms-2 mb-0">Dummy</p>
-                            </a>
-                            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <li>
-                                    <Link className="dropdown-item" aria-current="page" to="/profile">Profile</Link>
-                                </li>
-                                <li><a className="dropdown-item" href="#">Switch Mode</a></li>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><button className="dropdown-item" onClick={handleLogout}>Sign Out</button></li>
-                            </ul>
-                        </li>
-                    </ul>
+                    { user &&
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                            </svg>
+                            <li className="nav-item">
+                                <button className="btn btn-danger mt-2" onClick={handleLogout}  >Logout</button>
+                            </li>
+                        </ul>
+                        
+
+                    }
+                    { !user &&
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/register">Register</Link>
+                            </li>
+                        </ul>
+                    }
+
                 </div>
             </div>
         </nav>
-    );
+    )
 }
