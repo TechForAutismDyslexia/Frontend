bc413a1047eb6b00a85cf49044c04a4a11fdf0a8
Varun-MSI <tvarun2014@gmail.com>
2024-07-02 23:18:17 +0530

    New

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,23 +6,35 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
     const [user, setUser] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
+
     useEffect(() => {
         const token = sessionStorage.getItem('logintoken');
         if (token) {
             setUser(true);
         }
     }, [location]);
+
+    const handleModal = () => {
+        setOpenModal(true);
+    }
+    const closeModal = () => {
+        setOpenModal(false);
+    }
+
     const handleLogout = () => {
+        setOpenModal(false);
         sessionStorage.removeItem('logintoken');
         sessionStorage.removeItem('role');
         sessionStorage.removeItem('id');
-        navigate('/login');
         setUser(false);
+        navigate('/login');
     };
     return (
-        <nav className="navbar navbar-expand-lg">
+        // style={{backgroundColor:'#76c2f5'}}
+        <nav className="navbar navbar-expand-lg">     
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
                     <img src={logo} style={{borderRadius:'40px'}} alt="Logo" height='50' width='50' />
@@ -50,7 +62,7 @@ export default function Navbar() {
                                 <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                             </svg>
                             <li className="nav-item">
-                                <button className="btn btn-danger mt-2 ms-3" onClick={handleLogout}  >Logout</button>
+                                <button className="btn btn-danger mt-2 ms-3" onClick={handleModal}  >Logout</button>
                             </li>
                         </ul>
                         
@@ -65,6 +77,27 @@ export default function Navbar() {
                                 <Link className="nav-link" to="/register">Register</Link>
                             </li>
                         </ul>
+                    }
+                    {openModal &&
+                        <div className={`modal fade ${openModal ? 'show' : ''}`} style={{ display: openModal ? 'block' : 'none' }} tabIndex="-1" role="dialog" aria-hidden={!openModal}>
+                        <div className="modal-dialog modal-dialog-centered">
+                          <div className="modal-content">
+                            <div className="modal-header" style={{backgroundColor:'#f57676'}}>
+                              <h5 className="modal-title">Logout</h5>
+                              <button type="button" className="close btn-large btn" onClick={closeModal} aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div className="modal-body">
+                              Are you sure you want to logout?
+                            </div>
+                            <div className="modal-footer">
+                              <button type="button" className="btn btn-secondary" onClick={closeModal}>Close</button>
+                              <button type="button" className="btn btn-danger" onClick={handleLogout}>Logout</button>
+                              </div>
+                          </div>
+                        </div>
+                      </div>
                     }
 
                 </div>
