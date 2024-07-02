b6e667cfffbe3ef077ebfef112950a8a2cf22bf0
Varun-MSI <tvarun2014@gmail.com>
2024-07-02 15:58:56 +0530

    Admin Portal

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : '/adminportal',
   plugins: [react()],
-  homepage : '/adminportal',
   server : {
     port : 3000
   }
