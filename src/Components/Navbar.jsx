e40fa7ba3eddd3406b0c725dccee5bba4bfd1459
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2024-07-03 19:45:27 +0530

    navbar made better

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,10 +4,11 @@ import logo from './images/family.png'
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
+import {  NavDropdown } from 'react-bootstrap';
 export default function Navbar() {
     const [user, setUser] = useState(false);
     const [openModal, setOpenModal] = useState(false);
-    const [username,setUsername] = useState('');
+    const [username, setUsername] = useState('');
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -37,10 +38,10 @@ export default function Navbar() {
     };
     return (
         // style={{backgroundColor:'#76c2f5'}}
-        <nav className="navbar navbar-expand-lg border border-2 border-dark rounded-4 m-3 fs-5" style={{backgroundColor:'rgb(100, 150, 200)'}}>
+        <nav className="navbar navbar-expand-lg border border-2 border-dark rounded-4 m-3 fs-6" style={{ backgroundColor: 'rgb(100, 150, 200)' }}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
-                    <img src={logo} style={{ borderRadius: '40px' }} className='border border-dark border-2' alt="Logo" height='50' width='50' />
+                    <img src={logo} style={{ borderRadius: '40px' }} className='border border-dark border-2' alt="Logo" height='40' width='40' />
                     <span className='ms-4'>JWL</span>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -60,7 +61,7 @@ export default function Navbar() {
                     </ul>
                     {user &&
                         <div>
-                            <ul className="navbar-nav  dropstart">
+                            {/* <ul className="navbar-nav  dropstart">
 
                                 <li className="nav-item ">
                                     <button className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -79,7 +80,26 @@ export default function Navbar() {
                                     </ul>
                                 </li>
 
-                            </ul>
+                            </ul> */}
+
+                            <NavDropdown className='p-0'
+                                title={
+                                   
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="black" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                                        </svg>
+
+                                  
+                                }
+                                id="basic-nav-dropdown"
+                                alignRight
+                            >
+                                <NavDropdown.Item className='fw-bold'>{username}</NavDropdown.Item>
+                                <NavDropdown.Item onClick={handleLogout} className='text-danger fw-bolder'>Logout</NavDropdown.Item>
+                            </NavDropdown>
+
+
                         </div>
 
                     }
