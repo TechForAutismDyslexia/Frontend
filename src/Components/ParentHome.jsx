b71cc978d163feace1cd086fb169de6b8e52ae21
Varun-MSI <tvarun2014@gmail.com>
2024-07-03 13:15:25 +0530

    New

--- src/Components/ParentHome.jsx
+++ src/Components/ParentHome.jsx
@@ -4,32 +4,27 @@ import Profile from './images/profile.png';
 import axios from 'axios';
 import Loader from './Loader.jsx';
 import { Link } from 'react-router-dom';
-export default function ParentHome() {
 
-  const [responses, setResponses] = useState('')
+export default function ParentHome() {
+  const [responses, setResponses] = useState('');
 
   const b = async () => {
-
-    const response = await axios.get('https://jwlgamesbackend.vercel.app/api/parent/children',
-      {
-        headers: {
-          'Authorization': `${sessionStorage.getItem('logintoken')}`
-        }
-      }
-    )
-    setResponses(response.data)
-    console.log(response.data)
-  }
-
+    const response = await axios.get('https://jwlgamesbackend.vercel.app/api/parent/children', {
+      headers: {
+        Authorization: `${sessionStorage.getItem('logintoken')}`,
+      },
+    });
+    setResponses(response.data);
+    console.log(response.data);
+  };
 
   useEffect(() => {
     b();
-  }, [])
+  }, []);
 
   const handleClick = (response) => {
-    sessionStorage.setItem('childId', response._id)
-  }
-
+    sessionStorage.setItem('childId', response._id);
+  };
 
   return (
     <div>
@@ -38,25 +33,54 @@ export default function ParentHome() {
           <h2>Welcome to the Parent Portal</h2>
         </section>
         <div>
-          <section className="my-4 row justify-content-center">
+          <section className="card-container">
             {!responses && <Loader />}
-            {responses && responses.map((response, index) => (
-              <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 text-center" style={{fontSize:'20px'}}>
-                <Link to="/parentdashboard/details" style={{textDecoration:'none'}} onClick={()=>handleClick(response)}>
-                <div className="d-flex justify-content-center p-3">
-                  <img src={Profile} className='img-fluid profile' alt="Profile"></img>
-                </div>
-                <h3><strong>Name : </strong>{response.name}</h3>
-                <p><strong>Age : </strong>{response.age}</p>
-                <p><strong>Parent Details : </strong>{response.parentDetails}</p>
-                <p><strong>Caretaker Name : </strong>{response.caretakerName}</p>
-                <p><strong>Doctor Name : </strong>{response.doctorName}</p>
-                <p><strong>Center Id : </strong>{response.centreId}</p>
-                <p><strong>Games Completed : </strong>{response.gamesCompleted}</p>
-                <p><strong>Admin Status : </strong>{response.adminStatus ? "true" : "false"}</p>
-            </Link>
-              </div>
-            ))}
+            {responses &&
+              responses.map((response, index) => (
+                <Link
+                  key={index}
+                  to="/parentdashboard/details"
+                  className="btn"
+                  onClick={() => handleClick(response)}
+                >
+                  <div className="card">
+                    <div className="d-flex justify-content-center p-3">
+                      <img src={Profile} className="img-fluid profile" alt="Profile"></img>
+                    </div>
+                    <div className="card-body">
+                      <h3 className="card-title"><strong>{response.name}</strong></h3>
+                      <p>
+                        <strong>Age : </strong>
+                        {response.age}
+                      </p>
+                      <p>
+                        <strong>Parent Details : </strong>
+                        {response.parentDetails}
+                      </p>
+                      <p>
+                        <strong>Caretaker Name : </strong>
+                        {response.caretakerName}
+                      </p>
+                      <p>
+                        <strong>Doctor Name : </strong>
+                        {response.doctorName}
+                      </p>
+                      <p>
+                        <strong>Center Id : </strong>
+                        {response.centreId}
+                      </p>
+                      <p>
+                        <strong>Games Completed : </strong>
+                        {response.gamesCompleted}
+                      </p>
+                      <p>
+                        <strong>Admin Status : </strong>
+                        {response.adminStatus ? 'true' : 'false'}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
           </section>
         </div>
       </div>
