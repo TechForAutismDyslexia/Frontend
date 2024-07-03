87bfc0e3507866a64af5fcabe9f34e77d13f65e5
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2024-07-03 09:45:01 +0530

    navbar changes made

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Register from './Components/Register.jsx'
 import Caretaker from './Components/Caretaker.jsx'
 import Admin from './Components/Admin.jsx'
+import Games from './Components/Details.jsx'
 import Doctor from './Components/Doctor.jsx'
 import './Components/Admin.css';
-import Details from './Components/Details.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { useState } from 'react';
 // import Profile from './Components/profile.jsx'
 
 function App() {
+  const [sharedUsername , setSharedUsername] = useState('');
+  const updateUsername = (username) => {
+    setSharedUsername(username);
+  }
 
   return (
     <>
       <Router basename="/adminportal">
-        <Navbar/>
+        <Navbar username = {sharedUsername} set = {setSharedUsername}/>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login update={updateUsername}/>} />
           <Route path="/parentdashboard" element={<ParentHome />} />
           <Route path="/register" element={<Register/>}/>
           <Route path="/caretakerdashboard" element={<Caretaker/>}/>
           <Route path="/admindashboard" element={<Admin/>}/>
-          <Route path="/parentdashboard/details" element={<Details/>}/>
+          <Route path="/parentdashboard/details" element={<Games/>}/>
           <Route path="/doctordashboard" element={<Doctor/>}/>
           {/* <Route path="/profile" element={<Profile/>}/> */}
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,11 +7,13 @@ import { useNavigate } from 'react-router-dom';
 export default function Navbar() {
     const [user, setUser] = useState(false);
     const [openModal, setOpenModal] = useState(false);
+    const [username,setUsername] = useState('');
     const location = useLocation();
     const navigate = useNavigate();
 
     useEffect(() => {
         const token = sessionStorage.getItem('logintoken');
+        setUsername(sessionStorage.getItem('username'));
         if (token) {
             setUser(true);
         }
@@ -57,14 +59,25 @@ export default function Navbar() {
                     </ul>
                     {user &&
                         <div>
-                            <ul className="navbar-nav mb-2 mb-lg-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                                </svg>
-                                <li className="nav-item">
-                                    <button className="btn btn-danger mt-2" onClick={handleModal}  >Logout</button>
+                            <ul className="navbar-nav  dropstart">
+
+                                <li class="nav-item ">
+                                    <button class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                                        </svg>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <p className='fw-bold ps-2 pt-2'>{username}</p>
+                                        </li>
+                                        <li>
+                                            <button onClick={handleLogout} className='btn pt-0 text-danger'> logout</button>
+                                        </li>
+                                    </ul>
                                 </li>
+
                             </ul>
                         </div>
 
