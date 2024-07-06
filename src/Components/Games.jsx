a71d166cbacbd583e8e15e3b9d818ccae0b68ef7
Varun-MSI <tvarun2014@gmail.com>
2024-07-06 11:28:37 +0530

    gameId

--- src/Components/Games.jsx
+++ src/Components/Games.jsx
@@ -4,6 +4,7 @@ import GameData from './gameData.json';
 export default function Games() {
   const [icons, setIcons] = useState([]);
 
+
   useEffect(() => {
     const loadIcons = async () => {
       const iconsArray = await Promise.all(
@@ -17,14 +18,18 @@ export default function Games() {
     loadIcons();
   }, []);
 
+  const setGameId = (id) =>{
+    sessionStorage.setItem('gameId',id);
+  }
+
   return (
     <div className='p-3'>
       <div className="container">
         <div className="row">
           {icons.slice(0, 13).map((icon, index) => (
             <div className="col-sm-6 col-md-4 col-lg-3 mb-4" key={index}>
-              <a href={icon.route} target='_blank' className="card-link" style={{textDecoration:'none'}}>
-                <div className="card" style={{borderRadius:'30px'}}>
+              <a href={icon.route} target='_blank' className="card-link"  style={{textDecoration:'none'}}>
+                <div className="card" style={{borderRadius:'30px'}} onClick={()=>setGameId(icon.gameId)}>
                   <img
                     src={icon.url}  
                     className="card-img-top"
