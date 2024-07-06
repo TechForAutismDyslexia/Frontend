e8be6cc3a2a36489b91ff07d17ad721fef17f723
Deepakpottavatri06 <deepakpottavatri06@gmail.com>
2024-07-06 21:17:46 +0530

    caretaker feedback updated

--- src/Components/Reports.jsx
+++ src/Components/Reports.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { ProgressBar, Container, Row, Col, Card, Table, Form, Button } from 'react-bootstrap';
+import { Row, Col, Card, Table, Form, Button, Modal } from 'react-bootstrap';
+
+
+
 
 const Reports = () => {
   const [child, setChild] = useState(sessionStorage.getItem('childId'));
@@ -9,12 +12,18 @@ const Reports = () => {
   const [games, setGames] = useState([]);
   const [feedback, setFeedback] = useState('');
   const [childFeedback, setChildFeedback] = useState('');
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     if (child) {
       getData();
     }
   }, [child]);
+
+
 
   const getData = async () => {
     try {
@@ -66,13 +75,17 @@ const Reports = () => {
   };
 
   return (
-    <Container>
+    <div className='p-2' style={{overflowX:"hidden"}}>
       <Row className="justify-content-center mt-5">
-        <Col md={4}>
-          <Card>
+        <Col md={3}>
+          <Card className='d-block'>
             {/* Uncomment and adjust the below line if there's an avatar URL in your data */}
             {/* <Card.Img variant="top" src={childData.avatar} alt="Avatar" className="img-fluid rounded-circle mt-3 mx-auto" style={{ width: '150px' }} /> */}
             <Card.Body className="text-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+              </svg>
               <Card.Title>{childData.name}</Card.Title>
               <Card.Text>
                 <strong>Caretaker:</strong> {childData.caretakerName}<br />
@@ -82,11 +95,24 @@ const Reports = () => {
                 {/* <ProgressBar now={childData.progress} label={`${childData.progress}%`} /> */}
               </Card.Text>
             </Card.Body>
-          </Card>
-          <Card className="mt-3">
-            <Card.Header>Feedback</Card.Header>
-            <Card.Body>
-              <Form>
+            <Card.Footer className='text-center'>
+              <Button variant="primary" onClick={handleShow}>
+                give feedback
+              </Button>
+
+              <Modal
+                show={show}
+                onHide={handleClose}
+                aria-labelledby="contained-modal-title-vcenter"
+                keyboard={false}
+                backdrop="static"
+                centered
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title-vcenter">Feedback</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <Form onSubmit={handleSubmitFeedback}>
                 <Form.Group>
                   <Form.Label>Leave Feedback</Form.Label>
                   <Form.Control
@@ -94,17 +120,25 @@ const Reports = () => {
                     value={feedback}
                     onChange={(e) => setFeedback(e.target.value)}
                     placeholder="Enter your feedback"
+                    required
                   />
                 </Form.Group>
-                <Button className="mt-3" variant="primary" onClick={handleSubmitFeedback}>
+                <Button className="mt-3" variant="primary">
                   Submit
                 </Button>
               </Form>
-            </Card.Body>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </Card.Footer>
           </Card>
         </Col>
-        <Col md={8}>
-          <Card>
+        <Col md={7}>
+          <Card className='d-block'>
             <Card.Header>Recent Actions</Card.Header>
             <Table hover>
               <thead>
@@ -129,7 +163,7 @@ const Reports = () => {
           </Card>
         </Col>
       </Row>
-    </Container>
+    </div>
   );
 };
 
