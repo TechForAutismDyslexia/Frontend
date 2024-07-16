99c444903db4c95680a3b1fb5da61905cfd7599c
Varun-MSI <tvarun2014@gmail.com>
2024-07-16 21:36:07 +0530

    New

--- src/Components/Games.jsx
+++ src/Components/Games.jsx
@@ -3,36 +3,51 @@ import GameData from './gameData.json';
 
 export default function Games() {
   const [icons, setIcons] = useState([]);
-
+  const [groupId, setGroupId] = useState('1'); 
 
   useEffect(() => {
     const loadIcons = async () => {
-      const iconsArray = await Promise.all(
-        (GameData).map(async icon => {
-          const imageUrl = await import(`../assets/icons/${icon.img}.jpg`);
-          return { ...icon, url: imageUrl.default };
-        })
-      );
-      setIcons(iconsArray);
+      const groupData = GameData.find(group => group[groupId]);
+      if (groupData) {
+        const iconsArray = await Promise.all(
+          groupData[groupId].map(async icon => {
+            const imageUrl = await import(`../assets/icons/${icon.img}.jpg`);
+            return { ...icon, url: imageUrl.default };
+          })
+        );
+        setIcons(iconsArray);1
+      }
     };
     loadIcons();
-  }, []);
+  }, [groupId]);
 
-  const setGameId = (id) =>{
-    sessionStorage.setItem('gameId',id);
-    localStorage.setItem('gameId',id);
+  const setGameId = (id) => {
+    sessionStorage.setItem('gameId', id);
+    localStorage.setItem('gameId', id);
+  }
+
+  const handleGroupChange = (e) => {
+    setGroupId(e.target.value);
   }
 
   return (
     <div className='p-3'>
       <div className="container">
+        <div className="row mb-4">
+          <div className="col">
+            <select onChange={handleGroupChange} value={groupId} className="form-select">
+              <option value="1">SUCCESSIVE PROCESSING </option>
+              <option value="2">SIMULTANEOUS PROCESSING </option>
+            </select>
+          </div>
+        </div>
         <div className="row">
-          {icons.slice(0, 14).map((icon, index) => (
+          {icons.map((icon, index) => (
             <div className="col-sm-6 col-md-4 col-lg-3 mb-4" key={index}>
-              <a href={icon.route} target='_blank' className="card-link"  style={{textDecoration:'none'}}>
-                <div className="card" style={{borderRadius:'30px'}} onClick={()=>setGameId(icon.gameId)}>
+              <a href={icon.route} target='_blank' className="card-link" style={{ textDecoration: 'none' }}>
+                <div className="card" style={{ borderRadius: '30px' }} onClick={() => setGameId(icon.gameId)}>
                   <img
-                    src={icon.url}  
+                    src={icon.url}
                     className="card-img-top"
                     alt={icon.name}
                     style={{ padding: '40px' }}
